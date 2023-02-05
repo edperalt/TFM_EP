7ece1a8de86ec2223e85614723f32ad7b707b170
edperalt <edperalt@gmail.com>
2023-02-05 23:53:18 +0400

    Create 000-propuesta_TFM.R
    la porpuesta del master con indice

---
+++ scripts/000-propuesta_TFM.R
@@ -0,0 +1,25 @@
+Se propone un TFM basado en un concurso realizado en la plataforma  XEEK (https://xeek.ai/challenges) . En el año 2020 realizaron un concurso para atacar un problema recurrente en la industria del petróleo, la predicción de Litologia (tipos de roca) en pozos a partir de la interpretación de registros. Por razones económicas y operacionales normalmente en muy pocos pozos se extraen muestras de roca donde se conoce a ciencia cierta la litología, y  estos se utilizan para guiar las interpretaciones en los pozos vecinos.  Este tipo de interpretación aun hoy es un ejercicio predominantemente manual.   
+Actualmente existe una gran resistencia en nuestra industria a las tecnologías nuevas, sin embargo la velocidad a la que generamos datos y  las crecientes necesidades energéticas, así como la mejoría de la capacidad computacional, cada vez mas nos abre la ventana para empezar a utilizar este tipo de herramientas en la industria.
+Este proyecto representa el ejercicio que realiza un geologo cuando esta estudiando un yacimiento, y es un proceso que se debe actualizar con cada pozo perforado. Actualmente trabajo en varios campos  el que se perforan un par de pozos al mes, y este tipo de tecnologías pueden ser d egran ayuda para tener primeras interpretaciones prácticamente en tiempo real.
+
+En conclusión 
+
+
+
+
+1.	1.	Descripción del problema
+2.	2.	Objetivos
+3.	3.	Análisis de los datos
+4.	a.	Datos disponibles
+5.	b.	Visualización de los datos
+6.	c.	Control de calidad y limpieza
+7.	4.	Estrategia
+8.	a.	Lógica detrás del plan
+9.	5.	Implementación
+10.	a.	Los datos faltantes (dañados o que no fueron adquiridos)
+11.	b.	Transformaciones e ingeniería de datos
+12.	c.	Modelos de predicción
+13.	d.	Comparativa de resultados
+14.	e.	Selección del modelo ganador
+15.	6.	Conclusiones
+16.	7.	Recomendaciones
